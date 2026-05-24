7c47a64d36558466581124aeae1f93471107690f
newbie12-drblk <newbie12@gmail.com>
2026-05-24 11:29:03 +0700

    Add SSL config for database connection

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,14 +3,25 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
+let sslConfig = null;
+
+// Nếu có DB_SSL_CA thì cấu hình SSL
+if (process.env.DB_SSL_CA) {
+  sslConfig = {
+    ca: process.env.DB_SSL_CA,
+  };
+}
+
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
+  ssl: sslConfig,
 });
 
 const promisePool = pool.promise();
