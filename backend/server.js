d342997f1aa665fb982767700e00b082ce405e28
newbie12-drblk <newbie12@gmail.com>
2026-07-08 14:17:37 +0700

    LAGOM WMS v2.0 - Hoan thien backend voi 2 role Admin va Quan ly

--- backend/server.js
+++ backend/server.js
@@ -9,8 +9,6 @@ const PORT = process.env.PORT || 3000;
 
 // CORS - ĐƠN GIẢN NHẤT
 app.use(cors());
-app.options("*", cors());
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
